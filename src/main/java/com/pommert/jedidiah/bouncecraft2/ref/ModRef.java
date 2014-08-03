435231981c5d3bbfb96ff9f541863a768d6f6933
Kneelawk <jedidiah.pommert@gmail.com>
2014-08-03 14:35:34 -0700

    Finish testing EGit's file update detection system
    These commits were for testing egit's file update detection system.

--- src/main/java/com/pommert/jedidiah/bouncecraft2/ref/ModRef.java
+++ src/main/java/com/pommert/jedidiah/bouncecraft2/ref/ModRef.java
@@ -1,6 +1,5 @@
 package com.pommert.jedidiah.bouncecraft2.ref;
 
-// Edited some eclipse user properties, lets see if this makes a difference...
 public class ModRef {
 	public static final String MOD_ID = "bouncecraft2";
 }
